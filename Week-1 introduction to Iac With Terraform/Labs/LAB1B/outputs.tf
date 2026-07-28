e879647211f280fa6154ae2f09f3b8f0e1fab2ff
Harry Junior Nunyuie <selormharry@gmail.com>
2026-07-28 09:07:02 -0700

    docs: update Terraform configuration for Lab 1B, including variable renaming, resource adjustments, and new security group and EC2 instance definitions

--- Week-1 introduction to Iac With Terraform/Labs/LAB1B/outputs.tf
+++ Week-1 introduction to Iac With Terraform/Labs/LAB1B/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.selorm_vpc.id
+  description = "The ID of the created Virtual Private Cloud"
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "ec2_public_ip" {
+  value       = aws_instance.my_ec2.public_ip
+  description = "The public IP address to connect to your new EC2 instance via SSH"
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "security_group_id" {
+  value       = aws_security_group.ec2_sg.id
+  description = "The ID of the security group firewall attached to your instance"
 }
